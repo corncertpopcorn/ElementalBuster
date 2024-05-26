65402a79d5f7f084a176c4fa00ee5bbd7be59c14
dhsorkt <dhsorkt1@naver.com>
2024-05-26 14:13:42 +0900

    레포지토리 수정

--- src/main/java/org/example/chaos_dice_project/repository/AllRepository.java
+++ src/main/java/org/example/chaos_dice_project/repository/AllRepository.java
@@ -1,12 +1,15 @@
 package org.example.chaos_dice_project.repository;
 
+import org.example.chaos_dice_project.entity.PostEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface AllRepository extends JpaRepository<Post, String> {
-
+public interface AllRepository extends JpaRepository<PostEntity, String> {
+/*
     @Query(value = "SELECT COUNT(*) FROM post WHERE idx = :idx", nativeQuery = true)
     public int checkExist (@Param("idx") Integer idx);
+
+ */
 
 }
